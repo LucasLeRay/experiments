c2e22f060c1b85d4be63240d509d1fdb7288db0e
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-09-20 15:55:01 +0200

    Using operators

--- timely-dataflow/src/main.rs
+++ timely-dataflow/src/main.rs
@@ -1,13 +1,14 @@
 extern crate timely;
 
-use timely::dataflow::operators::{ToStream, Inspect};
+use timely::dataflow::operators::{ToStream, Map, Inspect};
 
 fn main() {
     timely::execute_from_args(std::env::args(), |worker| {
         worker.dataflow::<(),_,_>(|scope| {
             (0..10)
             .to_stream(scope)
-            .inspect_batch(|t, xs| println!("hello {:?} @ {:?}", xs, t));
+            .map(|x| x + 1)
+            .inspect(|x| println!("hello {}!", x));
         });
     }).unwrap();
 }
