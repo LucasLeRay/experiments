b409e9614527609078a26f0ad753ab60e911c91f
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-09-20 11:51:38 +0200

    Test inspect_batch

--- timely-dataflow/src/main.rs
+++ timely-dataflow/src/main.rs
@@ -3,9 +3,11 @@ extern crate timely;
 use timely::dataflow::operators::{ToStream, Inspect};
 
 fn main() {
-    timely::example(|scope| {
-        (0..10)
-        .to_stream(scope)
-        .inspect(|x| println!("seen: {:?}", x));
-    })
+    timely::execute_from_args(std::env::args(), |worker| {
+        worker.dataflow::<(),_,_>(|scope| {
+            (0..10)
+            .to_stream(scope)
+            .inspect_batch(|t, xs| println!("hello {:?} @ {:?}", xs, t));
+        });
+    }).unwrap();
 }
