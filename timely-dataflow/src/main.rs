97f561f8bae575a5246f26a12b8b27f50cd82eb6
Lucas Le Ray <lucas.a.leray@gmail.com>
2023-09-21 11:08:04 +0200

    Add custom operator

--- timely-dataflow/src/main.rs
+++ timely-dataflow/src/main.rs
@@ -1,14 +1,24 @@
 extern crate timely;
 
-use timely::dataflow::operators::{ToStream, Map, Inspect};
+use timely::dataflow::operators::{ToStream, Map, Inspect, Operator};
+use timely::dataflow::channels::pact::Pipeline;
 
 fn main() {
-    timely::execute_from_args(std::env::args(), |worker| {
-        worker.dataflow::<(),_,_>(|scope| {
-            (0..10)
-            .to_stream(scope)
-            .map(|x| x + 1)
-            .inspect(|x| println!("hello {}!", x));
+    timely::example(|scope| {
+        (0..10)
+        .to_stream(scope)
+        .unary(Pipeline, "increment", |capability, info| {
+            let mut vector = Vec::new();
+
+            move |input, output| {
+                while let Some((time, data)) = input.next() {
+                    data.swap(&mut vector);
+                    let mut session = output.session(&time);
+                    for datum in vector.drain(..) {
+                        session.give(datum + 1);
+                    }
+                }
+            }
         });
-    }).unwrap();
+    })
 }
